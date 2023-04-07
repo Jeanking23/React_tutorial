99d42b5e84b87f7ce8fdeb3ce107e5f1994760d5
Jeanking23 <jeandedieu405@gmail.com>
2023-04-07 15:30:08 -0400

    Complete AddEmntryForm Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { useState } from 'react';
-import DisplayEntries from './components/DisplayEntries'
+import DisplayEntries from './components/DisplayEntries';
+import AddEntryForm from './components/AddEntry/AddEntryForm';
 
 function App() {
-    const [entries, setEntries] = useState([{ weight: 175, date: "11-23-2021" }, { weight: 175, date: "11-23-2021" }]);
+    const [entries, setEntries] = useState([{ weight: 175, date: "11-23-2021" }, { weight: 175, date: "11-23-2021" }])
+
+    function addNewAddEntry(entry) {
+        let tempEntries = [...entries, entry];
+        setEntries(tempEntries);
+        entries.push(entry);
+    }
     return ( <
         div >
         <
         DisplayEntries parentEntries = { entries }
         setEntries = { setEntries }
+        /> <
+        AddEntryForm addNewAddEntryProperty = { addNewAddEntry }
         />
-
 
         <
         /div>
+
     );
-}
+};
 
 export default App;
---
+++ src/components/AddEntry/AddEntryForm.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+const AddEntryForm = (props) => {
+    const [weight, setWeight] = useState(0);
+    const [date, setDate] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        let newEntry = {
+            weight: weight,
+            date: date
+        };
+        console.log(newEntry);
+
+        props.addNewAddEntryProperty(newEntry);
+    };
+
+    return ( <
+        form onSubmit = { handleSubmit } >
+        <
+        label > Weight < /label> <
+        input type = "number"
+        value = { weight }
+        onChange = {
+            (event) => setWeight(event.target.value)
+        }
+        />
+
+        <
+        label > Date < /label> <
+        input type = "date"
+        value = { date }
+        onChange = {
+            (event) => setDate(event.target.value)
+        }
+        />
+
+        <
+        button type = "submit" > Add < /button> < /
+        form >
+    )
+};
+
+export default AddEntryForm;
